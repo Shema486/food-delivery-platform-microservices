46c4bbb9a769afeb4468b221fdca8841312e1561
Shema Alphonse <ashema330@gmail.com>
2026-02-28 10:30:24 +0200

    feat: add CustomerResponse DTO for external delivery service

---
+++ delivery-service/src/main/java/delivery_service/dto/external/CustomerResponse.java
@@ -0,0 +1,14 @@
+package delivery_service.dto.external;
+
+import lombok.Data;
+
+@Data
+public class CustomerResponse {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String deliveryAddress;
+    private String city;
+}
